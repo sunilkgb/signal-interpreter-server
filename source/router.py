d3b937c6a9140c2b95e265639fa11df63fae5d61
Pontus Sandh <pontus.sandh@consid.se>
2021-06-10 23:11:36 +0200

    removed dead code, added whitespace

--- source/router.py
+++ source/router.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from source.json_parser import JsonParser
 
 json_parser = JsonParser()
@@ -9,12 +9,9 @@ my_signal_interpreter_app = Flask(__name__)
 def hello():
     return "Hello world!"
 
+
 @my_signal_interpreter_app.route("/", methods=["POST"])
 def interpret_signal():
     data = request.get_json()
     signal_title = json_parser.get_signal_title(data["signal"])
     return jsonify(signal_title)
-    # return str(data["signal"])
-
-
-#my_signal_interpreter_app.run()
