3abc14f084d480e937bb37f07a676b2f2e35b73a
Pontus Sandh <pontus.sandh@consid.se>
2021-06-10 23:13:53 +0200

    added comment on test json

--- test/test_json_parser.py
+++ test/test_json_parser.py
@@ -7,3 +7,5 @@ def test_get_signal_title():
         json_parser = JsonParser
         json_parser.data = {"services": [{"title": "ECU Reset", "id": "11"}]}
         assert json_parser.get_signal_title("11") == "ECU Reset"
+        
+# TODO: Missing test load function
